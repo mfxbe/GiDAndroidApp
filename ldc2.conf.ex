0f79e87bfc03f1f1dd4f3a79e22039d438078325
Manuel Ehrmanntraut <dev@mfxbe.de>
2026-05-28 18:49:48 +0200

    Update ldc2.conf.ex

--- ldc2.conf.ex
+++ ldc2.conf.ex
@@ -1,13 +1,10 @@
-//The LDC2 config must be prepaired for android cross compiling see https://wiki.dlang.org/Build_D_for_Android
-//Append to the file:
-
-"aarch64-.*-linux-android31":
+"aarch64-.*-linux-android":
 {
     switches = [
         "-defaultlib=phobos2-ldc,druntime-ldc",
-        "-gcc=ANDROID/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android31-clang",
+        "-gcc=ANDOIDSDK/Sdk/ndk/30.0.14904198/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android31-clang",
     ];
     lib-dirs = [
-        "ANDROID/lib-android_aarch64",
+        "%%ldcbinarypath%%/../lib-android_aarch64",
     ];
 };
